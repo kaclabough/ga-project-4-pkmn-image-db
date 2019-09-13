581fdccab9ffedae13ecb05d68664a7b9f3e0271
Kyle Clabough <kaclabough@gmail.com>
2019-09-13 12:46:29 -0400

    started on webpage

---
+++ src/component/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">Who's That Pokemon?</h1>
+        <div className="pokeball-top" />
+        <button className="Open" />;
+        <div className="pokeball-bottom" />
+      </div>
+    );
+  }
+}
